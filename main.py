fcfe19fb417fd0371d96f1869d2c3e94112c07bf
Matheus Peixoto <peixoto2609@gmail.com>
2025-04-17 17:50:19 -0300

    Versao2

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def buscar_por_preco(lista):
 
 def buscar_por_estoque(lista):
     estoque_min = int(input("Digite a quantidade mínima em estoque: "))
-    encontrados = [livro for livro in lista if livro.estoque > estoque_min]
+    encontrados = []
     for livro in lista:
         if livro.estoque > estoque_min:
             encontrados.append(livro)
@@ -76,6 +76,28 @@ def valor_total_estoque(lista):
     total = sum(livro.valor * livro.estoque for livro in lista)
     print(f"O valor total em estoque é: R${total:.2f}\n")
 
+def carregar_estoque(lista):   
+    with open('base_dados.txt', 'r', encoding='utf-8') as arquivo:
+        for linha in arquivo:
+            partes = linha.strip().split(',')
+            if len(partes) == 7:
+                codigo = partes[0]
+                titulo = partes[1]
+                ano = partes[2]
+                area = partes[3]
+                editora = partes[4]
+                valor = float(partes[5].replace('R$', '').replace(',','.'))
+                estoque = int(partes[6])
+                lista.append(Livros(codigo, titulo, ano, area, editora, valor, estoque))
+    print('Estoque carregado com sucesso!')
+
+def atualizar_arquivo(lista):
+    with open('base_dados.txt', 'w', encoding='utf-8') as arquivo:
+        for livro in lista:
+            linha = f'{livro.codigo},{livro.titulo},{livro.ano},{livro.area},{livro.editora},R${livro.valor:.2f},{livro.estoque}\n'
+            arquivo.write(linha)
+    print('Dados salvos com sucesso!')
+
 def menu(lista):
     print()
     while True:
@@ -87,6 +109,8 @@ def menu(lista):
         print(f'5 - Buscar livros por preço')
         print(f'6 - Busca por quantidade em estoque')
         print(f'7 - Valor total em estoque')
+        print(f'8 - Carregar estoque')
+        print(f'9 - Atualizar arquivo de estoque')
         print(f'0 - Encerrar atividades')
         print()
 
@@ -107,15 +131,19 @@ def menu(lista):
             buscar_por_estoque(lista)
         elif opcao == '7':
             valor_total_estoque(lista)
+        elif opcao == '8':
+            carregar_estoque(lista)
+        elif opcao == '9':
+            atualizar_arquivo(lista)
         elif opcao == '0':
+            salvar = input("Deseja atualizar o arquivo de estoque antes de sair? (sim/não): ").lower()
+            if salvar == 'sim':
+                atualizar_arquivo(lista)
             print("Encerrando o programa...")
             break
         else:
             print("Opção inválida! Tente novamente.")
 
 if __name__ == '__main__':
-    lista = [
-        Livros('0301', 'Compiladores', 'Pearson', 'Computação', 2016, 85.00, 125),
-        Livros('1203', 'Engenharia de Software', 'Pressman', 'Computação', 2011, 78, 100)
-    ]
+    lista = []
     menu(lista)
